b3d45eb04b8613d1741a297555a48225eec3aacd
rezastein <rezastein531@gmail.com>
2024-09-26 00:27:56 +0700

    First Commit

---
+++ js/sc.js
@@ -0,0 +1,16 @@
+document.getElementById("pushdata").onclick = function (e) {
+  let name = document.getElementById("names").value;
+  let domi = document.getElementById("domi").value;
+  let notelp = document.getElementById("numbers").value;
+  let agent = document.getElementById("agent").value;
+  let qr = name + "-" + domi + "-" + notelp + "-" + agent;
+  genQr(qr);
+};
+let imgBox = document.getElementById("imgBox");
+let qrImg = document.getElementById("qrImg");
+
+function genQr(inputText) {
+  qrImg.src =
+    "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +
+    inputText;
+}
